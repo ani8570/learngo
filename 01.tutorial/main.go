2666b960979e4a57eec90da778df2effb6369e5a
ani8570 <ani8570@naver.com>
2021-02-21 02:54:59 +0900

    1

--- 01.tutorial/main.go
+++ 01.tutorial/main.go
@@ -77,50 +77,60 @@ type person struct {
 	favFood []string
 }
 
+func calc(f func(int, int) int, a int, b int) int {
+	result := f(a, b)
+	return result
+}
+func calc2(f calculator, a, b int) int {
+	result := f(a, b)
+	return result
+}
+
+type calculator func(int, int) int
+
 func main() {
-	name := "nico"
-	name = "Lynn"
-	fmt.Println(name)
-	fmt.Println(multyply(2, 2))
-
-	totalLength, upperName := lenAndUpper("Lee")
-	fmt.Println(totalLength, upperName)
-
-	totalLength, upperName = lenAndUpper2("Lee")
-	fmt.Println(totalLength, upperName)
-
-	totalLength, upperName = lenAndUpper3("Lee")
-	fmt.Println(totalLength, upperName)
-
-	repeatMe("nico", "lynn", "dal", "marl", "flynn")
-
-	result := superAdd(1, 2, 3, 4, 5, 6)
-	fmt.Println(result)
-
-	fmt.Println(canIDrink2(16))
-
-	a := 2
-	b := &a
-	a = 10
-	fmt.Println(&a, b)
-	fmt.Println(*b)
-
-	names1 := [5]string{"1", "2", "3", "4", "5"}
-	names2 := []string{"nico", "lynn", "dal", "marl", "flynn"}
-	names2 = append(names2, "lalalal")
-	fmt.Println(names1)
-	fmt.Println(names2)
-
-	Lee := map[string]string{"name": "L", "age": "12"}
-	fmt.Println(Lee)
-
-	for key, value := range Lee {
-		fmt.Println(key, value)
-	}
-
-	favFood := []string{"kimchi", "ramen"}
-	l := person{"Lee", 25, favFood}
-	i := person{name: "Lee", age: 18, favFood: favFood}
-	fmt.Println(l)
-	fmt.Println(i)
+	// name := "nico"
+	// name = "Lynn"
+	// fmt.Println(name)
+	// fmt.Println(multyply(2, 2))
+	// totalLength, upperName := lenAndUpper("Lee")
+	// fmt.Println(totalLength, upperName)
+	// totalLength, upperName = lenAndUpper2("Lee")
+	// fmt.Println(totalLength, upperName)
+	// totalLength, upperName = lenAndUpper3("Lee")
+	// fmt.Println(totalLength, upperName)
+	// repeatMe("nico", "lynn", "dal", "marl", "flynn")
+	// result := superAdd(1, 2, 3, 4, 5, 6)
+	// fmt.Println(result)
+	// fmt.Println(canIDrink2(16))
+	// a := 2
+	// b := &a
+	// a = 10
+	// fmt.Println(&a, b)
+	// fmt.Println(*b)
+	// names1 := [5]string{"1", "2", "3", "4", "5"}
+	// names2 := []string{"nico", "lynn", "dal", "marl", "flynn"}
+	// names2 = append(names2, "lalalal")
+	// fmt.Println(names1)
+	// fmt.Println(names2)
+	// Lee := map[string]string{"name": "L", "age": "12"}
+	// fmt.Println(Lee)
+	// for key, value := range Lee {
+	// 	fmt.Println(key, value)
+	// }
+	// favFood := []string{"kimchi", "ramen"}
+	// l := person{"Lee", 25, favFood}
+	// i := person{name: "Lee", age: 18, favFood: favFood}
+	// fmt.Println(l)
+	// fmt.Println(i)
+	// add := func(i int, j int) int {
+	// 	return i + j
+	// }
+	// r1 := calc(add, 10, 20)
+	// r2 := calc(func(x, y int) int { return x - y }, 10, 20)
+	// r3 := calc2(func(x, y int) int { return x - y }, 10, 20)
+	// fmt.Println(r1)
+	// fmt.Println(r2)
+	// fmt.Println(r3)
+	fmt.Println(123)
 }
